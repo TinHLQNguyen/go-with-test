fae2237dc465bc99c0249969ce7302ff2c9f1b48
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-28 11:50:33 +0900

    refactor test

--- my-project/hello_test.go
+++ my-project/hello_test.go
@@ -7,17 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Tin")
 		want := "Hello, Tin"
 
-		if got != want {
-			t.Errorf("go %q , want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying 'Hello, World' when empty input", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("go %q , want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("go %q , want %q", got, want)
+	}
 }
