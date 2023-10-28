5d3ad297000f717d44c049c692006b25a13c5945
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-28 11:45:04 +0900

    add new function test and satisfied

--- my-project/hello_test.go
+++ my-project/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Tin")
-	want := "Hello, Tin"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Tin")
+		want := "Hello, Tin"
 
-	if got != want {
-		t.Errorf("go %q , want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("go %q , want %q", got, want)
+		}
+	})
+
+	t.Run("Saying 'Hello, World' when empty input", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("go %q , want %q", got, want)
+		}
+	})
 }
