0a9d64b64d43506fccc4a9986b6165f5c4fcd86c
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-28 23:07:51 +0900

    refactor subroutine greetingPrefix

--- my-project/hello.go
+++ my-project/hello.go
@@ -1,27 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const spanish = "Spanish"
-const spanishHelloPrefix = "Hola, "
-const french = "French"
-const frenchHelloPrefix = "Bonjour, "
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	prefix := greetingPrefix(language)
+
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
