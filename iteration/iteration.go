70194cec078e7bc7dec307fbfaa611f0932f2eb6
tin.nguyen <tin.nguyen@mujin.co.jp>
2023-11-01 19:57:20 +0900

    test commit

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -5,7 +5,7 @@ import "strings"
 const repeatCount = 5
 
 func Repeat(character string, count int) string {
-	var repeated string // only initialize with string's "zero" value
+	var repeated string // should only initialize with string's "zero" value
 	repeated = strings.Repeat(character, count)
 	return repeated
 }
