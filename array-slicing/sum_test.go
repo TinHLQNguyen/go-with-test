2ec924596e9e03d07263343e1f5d39549a1efcb4
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-04 00:33:50 +0900

    remove redundant test

--- array-slicing/sum_test.go
+++ array-slicing/sum_test.go
@@ -4,22 +4,11 @@ import "testing"
 
 // use given-when-then framework
 func TestSum(t *testing.T) {
-	t.Run("collection of fixed 5 numbers", func(t *testing.T) {
+	t.Run("collection of some slice numbers", func(t *testing.T) {
 		given := []int{1, 2, 3, 4, 5}
 
 		when := Sum(given)
 		then := 15
-
-		if when != then {
-			t.Errorf("when %d then %d, given %v", when, then, given)
-		}
-	})
-
-	t.Run("collection of any size", func(t *testing.T) {
-		given := []int{1, 2, 3}
-
-		when := Sum(given)
-		then := 6
 
 		if when != then {
 			t.Errorf("when %d then %d, given %v", when, then, given)
