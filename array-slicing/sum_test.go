0cfd56ae36e7da7913a19b40f2ce3a96d31f221a
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-04 00:30:48 +0900

    update test for slice input

--- array-slicing/sum_test.go
+++ array-slicing/sum_test.go
@@ -5,7 +5,7 @@ import "testing"
 // use given-when-then framework
 func TestSum(t *testing.T) {
 	t.Run("collection of fixed 5 numbers", func(t *testing.T) {
-		given := [5]int{1, 2, 3, 4, 5}
+		given := []int{1, 2, 3, 4, 5}
 
 		when := Sum(given)
 		then := 15
