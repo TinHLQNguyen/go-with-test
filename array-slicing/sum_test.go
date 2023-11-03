e853c1c35c17465fa0030208b10130803f9a0bca
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-04 00:28:57 +0900

    add test for slice (non fixed size array)

--- array-slicing/sum_test.go
+++ array-slicing/sum_test.go
@@ -4,12 +4,25 @@ import "testing"
 
 // use given-when-then framework
 func TestSum(t *testing.T) {
-	given := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of fixed 5 numbers", func(t *testing.T) {
+		given := [5]int{1, 2, 3, 4, 5}
 
-	when := Sum(given)
-	then := 15
+		when := Sum(given)
+		then := 15
 
-	if when != then {
-		t.Errorf("when %d then %d, given %v", when, then, given)
-	}
+		if when != then {
+			t.Errorf("when %d then %d, given %v", when, then, given)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		given := []int{1, 2, 3}
+
+		when := Sum(given)
+		then := 6
+
+		if when != then {
+			t.Errorf("when %d then %d, given %v", when, then, given)
+		}
+	})
 }
