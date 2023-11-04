905c890fe6960f77c35f56d208be7d0c8ee3525f
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-05 03:12:36 +0900

    test and signature for shape

---
+++ struct-method-interface/shape_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+// use given-when-then pattern
+func TestPerimeter(t *testing.T) {
+	when := Perimeter(10.0, 10.0)
+	then := 40.0
+
+	if when != then {
+		t.Errorf("when %.2f then %.2f", when, then)
+	}
+}
