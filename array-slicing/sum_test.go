272496d1a15c8356dda6e31d283f69dba57aeb93
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-05 02:24:49 +0900

    helper assert func

--- array-slicing/sum_test.go
+++ array-slicing/sum_test.go
@@ -33,7 +33,19 @@ func TestSumAll(t *testing.T) {
 	when := SumAll([]int{1, 2, 4}, []int{0, 9})
 	then := []int{7, 9}
 
+	assertCorrectSlice(t, when, then)
+}
+
+func TestSumAllTails(t *testing.T) {
+	when := SumAllTails([]int{1, 2, 5}, []int{0, 9})
+	then := []int{6, 9}
+
+	assertCorrectSlice(t, when, then)
+}
+
+func assertCorrectSlice(t testing.TB, when, then []int) {
+	t.Helper()
 	if !reflect.DeepEqual(when, then) {
-		t.Errorf("when %v , then %d", when, then)
+		t.Errorf("when %v , then %v", when, then)
 	}
 }
