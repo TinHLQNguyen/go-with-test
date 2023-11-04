e5e42df11ea77d6dd46b710b5ad559c6ef7bef87
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-05 02:46:54 +0900

    local function as a variable

--- array-slicing/sum_test.go
+++ array-slicing/sum_test.go
@@ -7,15 +7,19 @@ import (
 
 // use given-when-then framework
 func TestSum(t *testing.T) {
+	assertCorrectSum := func(t testing.TB, when, then int, given []int) {
+		t.Helper()
+		if !reflect.DeepEqual(when, then) {
+			t.Errorf("when %d then %d, given %v", when, then, given)
+		}
+	}
 	t.Run("collection of some slice numbers", func(t *testing.T) {
 		given := []int{1, 2, 3, 4, 5}
 
 		when := Sum(given)
 		then := 15
 
-		if when != then {
-			t.Errorf("when %d then %d, given %v", when, then, given)
-		}
+		assertCorrectSum(t, when, then, given)
 	})
 	t.Run("collection of any slice numbers", func(t *testing.T) {
 		given := []int{1, 2, 3}
@@ -23,9 +27,7 @@ func TestSum(t *testing.T) {
 		when := Sum(given)
 		then := 6
 
-		if when != then {
-			t.Errorf("when %d then %d, given %v", when, then, given)
-		}
+		assertCorrectSum(t, when, then, given)
 	})
 }
 
