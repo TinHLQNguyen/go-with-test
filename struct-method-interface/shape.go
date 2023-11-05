8aa1dd90d54513ded953f04c7b0ed2781dbf74b8
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-05 18:48:52 +0900

    make Perimeter into methods

--- struct-method-interface/shape.go
+++ struct-method-interface/shape.go
@@ -11,6 +11,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -19,6 +23,6 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectange Rectangle) float64 {
-	return 2 * (rectange.Width + rectange.Height)
+func (c Circle) Perimeter() float64 {
+	return math.Pi * 2 * c.Radius
 }
