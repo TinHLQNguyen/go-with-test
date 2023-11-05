52819bfabcb2f759cf491f610c02ab976404cc1c
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-05 17:49:20 +0900

    helper func for test

--- struct-method-interface/shape_test.go
+++ struct-method-interface/shape_test.go
@@ -2,15 +2,20 @@ package main
 
 import "testing"
 
+func assertCorrectValue(t testing.TB, when, then float64) {
+	t.Helper()
+	if when != then {
+		t.Errorf("when %.2f then %.2f", when, then)
+	}
+}
+
 // use given-when-then pattern
 func TestPerimeter(t *testing.T) {
 	given := Rectangle{10.0, 10.0}
 	when := Perimeter(given)
 	then := 40.0
 
-	if when != then {
-		t.Errorf("when %.2f then %.2f", when, then)
-	}
+	assertCorrectValue(t, when, then)
 }
 
 func TestArea(t *testing.T) {
@@ -18,7 +23,5 @@ func TestArea(t *testing.T) {
 	when := Area(given)
 	then := 100.0
 
-	if when != then {
-		t.Errorf("when %.2f then %.2f", when, then)
-	}
+	assertCorrectValue(t, when, then)
 }
