67f9a5c8e0e275e31d623c81c27094066158f190
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-07 23:43:14 +0900

    refactor tests

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -3,28 +3,27 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, given Wallet, then Bitcoin) {
+		t.Helper()
+		when := given.Balance()
+		if when != then {
+			t.Errorf("Given %#v when %s then %s", given, when, then)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		given := Wallet{}
 
 		given.Deposit(Bitcoin(10))
-
-		when := given.Balance()
 		then := Bitcoin(10)
 
-		if when != then {
-			t.Errorf("Given %#v when %s then %s", given, when, then)
-		}
+		assertBalance(t, given, then)
 	})
 	t.Run("withdraw", func(t *testing.T) {
 		given := Wallet{balance: Bitcoin(20)}
 
 		given.Withdraw(Bitcoin(10))
-
-		when := given.Balance()
 		then := Bitcoin(10)
 
-		if when != then {
-			t.Errorf("Given %#v when %s then %s", given, when, then)
-		}
+		assertBalance(t, given, then)
 	})
 }
