41817092bf636d73d3ea16ea0be194706e84049e
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-07 23:32:07 +0900

    withdraw test

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -3,14 +3,28 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	given := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		given := Wallet{}
 
-	given.Deposit(Bitcoin(10))
+		given.Deposit(Bitcoin(10))
 
-	when := given.Balance()
-	then := Bitcoin(10)
+		when := given.Balance()
+		then := Bitcoin(10)
 
-	if when != then {
-		t.Errorf("Given %#v when %s then %s", given, when, then)
-	}
+		if when != then {
+			t.Errorf("Given %#v when %s then %s", given, when, then)
+		}
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		given := Wallet{balance: Bitcoin(20)}
+
+		given.withdraw(Bitcoin(10))
+
+		when := given.Balance()
+		then := Bitcoin(10)
+
+		if when != then {
+			t.Errorf("Given %#v when %s then %s", given, when, then)
+		}
+	})
 }
