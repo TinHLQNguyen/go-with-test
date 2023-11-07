b9bc1754a73bae259de609ad686a0a8652b6a0d0
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-07 23:07:00 +0900

    refactor, add domain-meaningful type

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,14 +1,17 @@
 package main
 
+// create type from generic one
+type Bitcoin int
+
 type Wallet struct {
 	// lowercase means private
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestWallet(t *testing.T) {
 	given := Wallet{}
 
-	given.Deposit(10)
+	given.Deposit(Bitcoin(10))
 
 	when := given.Balance()
-	then := 10
+	then := Bitcoin(10)
 
 	if when != then {
 		t.Errorf("Given %#v when %d then %d", given, when, then)
