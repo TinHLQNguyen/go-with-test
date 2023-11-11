781ec500118ba4cd599307274bf528ac79929540
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-11 22:43:21 +0900

    test typo

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -21,7 +21,7 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err = dictionary.Search("unknonw")
+		_, err := dictionary.Search("unknown")
 		then := "could not find the word you are looking for"
 
 		if err == nil {
