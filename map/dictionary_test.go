fc8464a13b0534b00d40aa4159ea72b7ab696b02
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-11 22:07:26 +0900

    add no key test case

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -13,9 +13,22 @@ func assertString(t testing.TB, when, then string) {
 func TestSearch(t *testing.T) {
 	// given
 	dictionary := Dictionary{"test": "this is a test"}
+	t.Run("known word", func(t *testing.T) {
+		when, _ := dictionary.Search("test")
+		then := "this is a test"
 
-	when := dictionary.Search("test")
-	then := "this is a test"
+		assertString(t, when, then)
+	})
 
-	assertString(t, when, then)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err = dictionary.Search("unknonw")
+		then := "could not find the word you are looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertString(t, err.Error(), then)
+	})
+
 }
