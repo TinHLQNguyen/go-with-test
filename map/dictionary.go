df7127b9c1a610b195c7963bf001729d73a653af
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-12 01:29:41 +0900

    add wordexist error

--- map/dictionary.go
+++ map/dictionary.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you are looking for")
+var (
+	ErrNotFound  = errors.New("could not find the word you are looking for")
+	ErrWordExist = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -15,6 +18,17 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // map type is already pointer, no need here
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+		return nil
+	case nil:
+		return ErrWordExist
+	default:
+		// this should be some unexpected err
+		return err
+	}
 }
