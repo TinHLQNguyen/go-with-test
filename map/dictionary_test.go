8d6d86b2e1dfbc48400fa63bd5ae89bd4a4f785f
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-12 03:04:00 +0900

    fix test

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -51,7 +51,7 @@ func TestUpdate(t *testing.T) {
 		dictionary := Dictionary{word: definition}
 		newDefinition := "new definition"
 
-		dictionary.Update(word, definition)
+		dictionary.Update(word, newDefinition)
 
 		assertDefinition(t, dictionary, word, newDefinition)
 	})
