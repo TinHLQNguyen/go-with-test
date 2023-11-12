0c5c276a770ac0cdfb18428d8d58cc8b901d045c
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-13 00:15:08 +0900

    add interface to mock Sleep behavior

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -12,6 +12,20 @@ const (
 	countdownStart = 3
 )
 
+// make a Sleeper interface using Sleep() to add dependency to the behavior
+type Sleeper interface {
+	Sleep()
+}
+
+// This is the mock Sleeper we use our tests to hasten it
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func CountDown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
