9b00726c734cd0f569e9b302fc812c2fbbc95519
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-11-16 23:10:56 +0900

    using channel

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -1,18 +1,28 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	// channel containing result type
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			// during goroutine, race condition happens here
-			results[u] = wc(u)
+			// concurrent writes can be sent to channel safely
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		// sequencially extract (receive) stuff inside the channel
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
