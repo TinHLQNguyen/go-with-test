dddfe8a714c00ab59d57dec90b97ca3faaf1377e
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-12-16 21:41:30 +0900

    naive way, pass string w/o validate input

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -11,10 +11,15 @@ func TestWalk(t *testing.T) {
 	}{expected}
 
 	walk(x, func(input string) {
+		// this is just a anonymous function
 		got = append(got, input)
 	})
 
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q want %q", got[0], expected)
 	}
 }
