6edc8dfc0d9ef9f01cf6f017b3895306d7d84f2c
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-12-24 20:36:41 +0900

    refactor to treat iterating struct and slice the same

--- reflection/walk.go
+++ reflection/walk.go
@@ -5,19 +5,25 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
+	numberOfValues := 0
+	// getField can be use for both reflect.Value.Field(i) or reflect.Value.Index(i)
+	var getField func(int) reflect.Value
+
 	switch val.Kind() {
 	// run recursively if meet with Slice
 	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
+		numberOfValues = val.Len()
+		getField = val.Index
 	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
+		numberOfValues = val.NumField()
+		getField = val.Field
 	// in this case, only 1 string in input
 	case reflect.String:
 		fn(val.String())
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
