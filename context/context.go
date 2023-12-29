d178b18c7bc83f42bf2b2ad635fef3e6c3f5a22a
tin.nguyen <tin.nguyen@mujin.co.jp>
2023-12-29 17:44:58 +0900

    add mock store with cancel

--- context/context.go
+++ context/context.go
@@ -3,18 +3,35 @@ package context
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
-type StubStore struct {
-	response string
+///// Stubstore cannot fulfil condition of Store interface anymore, use SpyStore for test instead
+// type StubStore struct {
+// 	response string
+// }
+
+// func (s *StubStore) Fetch() string {
+// 	return s.response
+// }
+
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func Server(store Store) http.HandlerFunc {
