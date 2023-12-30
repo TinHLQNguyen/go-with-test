e5f65b7ec93ab7ebfaf46a1aed4938231e810c9b
tin.nguyen <tin.nguyen@mujin.co.jp>
2023-12-30 22:08:06 +0900

    move mocking to test

--- context/context.go
+++ context/context.go
@@ -1,9 +1,8 @@
-package context
+package mycontext
 
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 type Store interface {
@@ -20,26 +19,13 @@ type Store interface {
 // 	return s.response
 // }
 
-type SpyStore struct {
-	response  string
-	cancelled bool
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
 		data := make(chan string, 1)
 
+		// running data in goroutine will race Fetch and Cancel signal
 		go func() {
 			data <- store.Fetch()
 		}()
