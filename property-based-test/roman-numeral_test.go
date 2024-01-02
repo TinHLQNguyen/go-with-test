bc74cf0473f3a37434b370a805b298b406cfeb6d
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-02 16:04:13 +0900

    add test for 4

--- property-based-test/roman-numeral_test.go
+++ property-based-test/roman-numeral_test.go
@@ -11,6 +11,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 get converted to I", 1, "I"},
 		{"2 get converted to II", 2, "II"},
 		{"3 get converted to III", 3, "III"},
+		{"4 get converted to IV (can't repeat more than 3)", 4, "IV"},
 	}
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
