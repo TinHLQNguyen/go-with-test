97bc352f4d5c9f1dc3e8906a5c674e01a05764af
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-02 15:34:02 +0900

    refactor test

--- property-based-test/roman-numeral_test.go
+++ property-based-test/roman-numeral_test.go
@@ -3,10 +3,23 @@ package romanNumeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 get converted to I", 1, "I"},
+		{"2 get converted to II", 2, "II"},
+	}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+			got := ConvertToRoman(test.Arabic)
+			want := test.Want
+
+			if got != want {
+				t.Errorf("got %q want %q", got, test.Want)
+			}
+		})
 	}
 }
