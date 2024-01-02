508ac3836efb08ca51390a76e54483c0f3c7ec10
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-02 18:06:39 +0900

    add and handle up to 10

--- property-based-test/roman-numeral_test.go
+++ property-based-test/roman-numeral_test.go
@@ -15,6 +15,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"6 get converted to VI ", 6, "VI"},
 		{"7 get converted to VII ", 7, "VII"},
 		{"8 get converted to VIII ", 8, "VIII"},
+		{"10 get converted to X ", 10, "X"},
 		{"4 get converted to IV (can't repeat more than 3)", 4, "IV"},
 		{"9 get converted to IX (can't repeat more than 3)", 9, "IX"},
 	}
