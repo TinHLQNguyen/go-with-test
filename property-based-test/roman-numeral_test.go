3f8b2a897370ef136ab34374173b4547e8db301f
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-03 18:24:23 +0900

    add test for convert to Arabic

--- property-based-test/roman-numeral_test.go
+++ property-based-test/roman-numeral_test.go
@@ -5,42 +5,43 @@ import (
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Roman  string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-		{100, "C"},
-		{90, "XC"},
-		{400, "CD"},
-		{500, "D"},
-		{900, "CM"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-		{3999, "MMMCMXCIX"},
-		{2014, "MMXIV"},
-		{1006, "MVI"},
-		{798, "DCCXCVIII"},
-		{1984, "MCMLXXXIV"},
-	}
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{90, "XC"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+	{1984, "MCMLXXXIV"},
+}
+
+func TestConvertingToRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 
@@ -48,7 +49,21 @@ func TestRomanNumerals(t *testing.T) {
 			want := test.Roman
 
 			if got != want {
-				t.Errorf("got %q want %q", got, test.Roman)
+				t.Errorf("got %q want %q", got, want)
+			}
+		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases[:1] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+
+			got := ConvertToArabic(test.Roman)
+			want := test.Arabic
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
 			}
 		})
 	}
