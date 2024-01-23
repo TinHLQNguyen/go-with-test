742c51c454b46d155926568cd40b80b3ba081576
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-24 00:23:35 +0900

    added more cases to test second hand, no float handling

--- math/clockface_test.go
+++ math/clockface_test.go
@@ -29,11 +29,34 @@ import (
 // }
 
 func TestSecondsInRadians(t *testing.T) {
-	thirtySeconds := time.Date(2024, time.January, 1, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := secondsInRadians(thirtySeconds)
-
-	if got != want {
-		t.Errorf("Got %v, want %v", got, want)
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
 	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+
+			want := c.angle
+			got := secondsInRadians(c.time)
+
+			if got != want {
+				t.Errorf("Got %v, want %v radians", got, want)
+			}
+		})
+	}
+
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(2024, time.January, 1, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("10:00:05")
 }
