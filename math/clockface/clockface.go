d8c6f19b97c8b73cb3367b4a0081fcaa0f85858a
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-27 01:17:19 +0900

    restructure to build

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -11,12 +11,16 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t` represented as a Point.
 func SecondHand(t time.Time) Point {
 	p := secondsHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scaling
-	p = Point{p.X, -p.Y}            // flip coordinate
-	p = Point{p.X + 150, p.Y + 150} // translate coordinate
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scaling
+	p = Point{p.X, -p.Y}                                      // flip coordinate
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate coordinate
 	return p
 }
 
