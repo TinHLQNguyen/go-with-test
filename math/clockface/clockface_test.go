cb5ef6662a6973e805f6c43efc358370d8d0dea2
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-27 15:05:51 +0900

    separate unit and acceptance test

--- math/clockface/clockface_test.go
+++ math/clockface/clockface_test.go
@@ -6,28 +6,6 @@ import (
 	"time"
 )
 
-// func TestSecondHandAtMidnight(t *testing.T) {
-// 	tm := time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-// 	want := clockface.Point{X: 150, Y: 150 - 90}
-// 	got := clockface.SecondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("Got %v, want %v", got, want)
-// 	}
-// }
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(2024, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, want %v", got, want)
-	}
-}
-
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
