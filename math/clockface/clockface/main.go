12a8b2845d09cb29742ededc7bdb43c20abe148b
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-29 22:52:39 +0900

    restructure code, split files

--- math/clockface/clockface/main.go
+++ math/clockface/clockface/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"go-with-test/math/clockface"
+	"os"
+	"time"
 )
 
 func main() {
-	// t := time.Now()
-	// sh := clockface.SecondHand(t)
-	// io.WriteString(os.Stdout, svgStart)
-	// io.WriteString(os.Stdout, bezel)
-	// io.WriteString(os.Stdout, secondHandTag(sh))
-	// io.WriteString(os.Stdout, svgEnd)
-}
-
-func secondHandTag(p clockface.Point) string {
-	return fmt.Sprintf(`<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+	t := time.Now()
+	clockface.SVGWriter(os.Stdout, t)
 }
--- math/clockface/clockface_test.go
+++ math/clockface/clockface_test.go
@@ -1,8 +1,6 @@
 package clockface
 
 import (
-	"bytes"
-	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -56,27 +54,6 @@ func TestSecondHandPoint(t *testing.T) {
 	}
 }
 
-func TestSGVWriterAtMidnight(t *testing.T) {
-	tm := simpleTime(0, 0, 0)
-
-	b := bytes.Buffer{}
-	SVGWriter(&b, tm)
-
-	svg := SVG{}
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	x2 := "150.000"
-	y2 := "60.000"
-
-	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
-			return
-		}
-	}
-
-	t.Errorf("Expected to find the second hand with x2 %+v and y2 %+v, in the SVG output %v", x2, y2, b.String())
-}
-
 func simpleTime(hours, minutes, seconds int) time.Time {
 	jpLocation := time.FixedZone("JST", +9*int(time.Hour/time.Second))
 	return time.Date(2024, time.January, 1, hours, minutes, seconds, 0, jpLocation)
@@ -94,32 +71,4 @@ func roughlyEqualFloat64(a, b float64) bool {
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) &&
 		roughlyEqualFloat64(a.Y, b.Y)
-}
-
-// SVG was generated 2024-01-27 15:41:29 by https://xml-to-go.github.io/ in Ukraine.
-// Struct for XML package to unmarshall stuff
-type SVG struct {
-	// the 3rd element of struct field is tag, used by packages that uses reflect
-	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
 }
