876d5417775a0c5c9db7302dd2fdd6d50ead5058
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-01-29 23:13:20 +0900

    refactor, add acceptance case

--- math/clockface/clockface_acceptance_test.go
+++ math/clockface/clockface_acceptance_test.go
@@ -4,53 +4,73 @@ import (
 	"bytes"
 	"encoding/xml"
 	"testing"
+	"time"
 )
-
-func TestSGVWriterAtMidnight(t *testing.T) {
-	tm := simpleTime(0, 0, 0)
-
-	b := bytes.Buffer{}
-	SVGWriter(&b, tm)
-
-	svg := SVG{}
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	x2 := "150.000"
-	y2 := "60.000"
-
-	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
-			return
-		}
-	}
-
-	t.Errorf("Expected to find the second hand with x2 %+v and y2 %+v, in the SVG output %v", x2, y2, b.String())
-}
 
 // SVG was generated 2024-01-27 15:41:29 by https://xml-to-go.github.io/ in Ukraine.
 // Struct for XML package to unmarshall stuff
 type SVG struct {
 	// the 3rd element of struct field is tag, used by packages that uses reflect
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
 	ViewBox string   `xml:"viewBox,attr"`
 	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
+func TestSGVWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			want := c.line
+
+			if !containsLine(want, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+			}
+		})
+	}
+
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if l == line {
+			return true
+		}
+	}
+	return false
 }
