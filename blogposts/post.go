f1335a55f29dda21130c04ff07b53e9e29d932cd
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-05-03 01:18:55 +0900

    support tags field

--- blogposts/post.go
+++ blogposts/post.go
@@ -9,11 +9,13 @@ import (
 const (
 	titleSeparator       = "Title: "
 	descriptionSeparator = "Description: "
+	tagSeparator         = "Tags: "
 )
 
 type Post struct {
 	Title       string
 	Description string
+	Tags        []string
 }
 
 func newPost(postFile io.Reader) (Post, error) {
@@ -26,12 +28,13 @@ func newPost(postFile io.Reader) (Post, error) {
 
 	titleLine := readMetaLine(titleSeparator)
 	descriptionLine := readMetaLine(descriptionSeparator)
+	tagsLine := strings.Split(readMetaLine(tagSeparator), ", ")
 
 	err := scanner.Err()
 	if err != nil {
 		return Post{}, err
 	}
 
-	post := Post{Title: titleLine, Description: descriptionLine} // 7 is for "Title "
+	post := Post{Title: titleLine, Description: descriptionLine, Tags: tagsLine}
 	return post, nil
 }
