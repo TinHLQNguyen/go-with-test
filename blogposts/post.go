1e468613773247df60903b0aae7981fffeb6be3e
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-05-02 20:36:26 +0900

    naive handle file content

--- blogposts/post.go
+++ blogposts/post.go
@@ -1,17 +1,29 @@
 package blogposts
 
-import "io"
+import (
+	"bufio"
+	"io"
+)
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
 func newPost(postFile io.Reader) (Post, error) {
-	postData, err := io.ReadAll(postFile)
+	scanner := bufio.NewScanner(postFile)
+
+	scanner.Scan()
+	titleLine := scanner.Text()
+
+	scanner.Scan()
+	descriptionLine := scanner.Text()
+
+	err := scanner.Err()
 	if err != nil {
 		return Post{}, err
 	}
 
-	post := Post{Title: string(postData)[7:]} // 7 is for "Title "
+	post := Post{Title: string(titleLine)[7:], Description: string(descriptionLine)[13:]} // 7 is for "Title "
 	return post, nil
 }
