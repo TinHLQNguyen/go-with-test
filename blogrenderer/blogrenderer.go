244413225f35ea00458d73f52830351e98adc997
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-05-08 03:50:28 +0900

    ugly solution

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,10 +1,33 @@
 package blogrenderer
 
 import (
+	"fmt"
 	"go-with-test/blogposts"
 	"io"
 )
 
 func Render(w io.Writer, p blogposts.Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
