61e9a44a0818bcb3dd9322861a5488e336bd6d65
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-11 07:37:02 +0900

    stack accepts generics to reduce duplication

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := new(Stack[int])
 
 		// Check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
@@ -20,9 +20,16 @@ func TestStack(t *testing.T) {
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// make sure we get the numbers we push in as numbers, not untyped interface{}
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
+		myStackOfStrings := new(Stack[string])
 
 		// Check stack is empty
 		AssertTrue(t, myStackOfStrings.IsEmpty())
