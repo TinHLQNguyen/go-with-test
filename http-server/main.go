1babb2e42d496ae65d236057ece3034e49a34c1f
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-13 22:32:53 +0900

    app also need a store to run

--- http-server/main.go
+++ http-server/main.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 )
 
+// kept empty and hard-coded response until one is implemented
+type InMemoryPlayerStore struct{}
+
+func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
