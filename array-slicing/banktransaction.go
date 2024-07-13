4fba7842cac19ec1e294f4b2c044dcc526835aec
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-13 17:25:45 +0900

    Improve signature

--- array-slicing/banktransaction.go
+++ array-slicing/banktransaction.go
@@ -6,15 +6,25 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, t Transaction) float64 {
-		if t.From == name {
-			currentBalance -= t.Sum
-		}
-		if t.To == name {
-			currentBalance += t.Sum
-		}
-		return currentBalance
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewTransaction(from, to Account, amount float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: amount}
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
+
+func applyTransaction(a Account, t Transaction) Account {
+	if t.From == a.Name {
+		a.Balance -= t.Sum
 	}
-	return Reduce(transactions, adjustBalance, 0.0)
+	if t.To == a.Name {
+		a.Balance += t.Sum
+	}
+	return a
 }
