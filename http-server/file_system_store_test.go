470cfb427a82dbfa6c5ea4487ce70f10d5e103ab
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-20 10:49:27 +0900

    test: replace db type to support write to file

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("get league from reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
       {"Name": "Abe", "Wins": 2},
       {"Name": "Ben", "Wins": 10}
       ]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
-
 		want := []Player{
 			{"Abe", 2},
 			{"Ben", 10},
 		}
-
 		assertLeague(t, got, want)
 
 		// 2nd read to make sure can GetLeague multiple times
@@ -29,17 +29,35 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
       {"Name": "Abe", "Wins": 2},
       {"Name": "Ben", "Wins": 10}
       ]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Abe")
-
 		want := 2
-
 		AssertEqual(t, got, want)
 	})
+}
+
+// func() is destructor
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpFile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpFile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpFile.Close()
+		os.Remove(tmpFile.Name())
+	}
+
+	return tmpFile, removeFile
 }
