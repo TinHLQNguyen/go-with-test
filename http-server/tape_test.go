50ab67341689473331a1c446fa138772e6ef6821
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-21 10:01:44 +0900

    test: old data are not cleared before writing new

---
+++ http-server/tape_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"testing"
+)
+
+func TestTape_Write(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	tape := &tape{file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, io.SeekStart)
+	newFileContent, _ := io.ReadAll(file)
+
+	got := string(newFileContent)
+	want := "abc"
+
+	AssertEqual(t, got, want)
+}
