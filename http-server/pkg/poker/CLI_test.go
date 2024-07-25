67483cad8f719c228e47094786ebf339273a88c2
tin.nguyen <tin.nguyen@mujin.co.jp>
2024-07-26 02:12:32 +0900

    refactor: DRY assertions

--- http-server/pkg/poker/CLI_test.go
+++ http-server/pkg/poker/CLI_test.go
@@ -12,43 +12,31 @@ import (
 func TestCLI(t *testing.T) {
 	dummyStdOut := &bytes.Buffer{}
 	t.Run("prompt user to enter number of player and start game that ends with Abe win", func(t *testing.T) {
-		numberOfPlayer := 7
-		winner := "Abe"
+		in := userSends("7", "Abe wins")
 
 		stdOut := &bytes.Buffer{}
-		in := strings.NewReader(fmt.Sprintf("%d\n%s wins\n", numberOfPlayer, winner))
 		game := &SpyGame{}
 
 		cli := poker.NewCLI(in, stdOut, game)
 		cli.PlayPoker()
 
-		got := stdOut.String()
-		poker.AssertEqual(t, got, poker.PlayerPrompt)
-
-		if game.StartedWith != numberOfPlayer {
-			t.Errorf("wanted game to start with %d, got %d", numberOfPlayer, game.StartedWith)
-		}
+		assertMessageSentToUser(t, stdOut, poker.PlayerPrompt)
+		assertGameStartedWith(t, game, 7)
+		assertGameFinishedWith(t, game, "Abe")
 	})
 	t.Run("start 7 players game and Chris win", func(t *testing.T) {
-		numberOfPlayer := 7
-		winner := "Chris"
-		in := strings.NewReader(fmt.Sprintf("%d\n%s wins\n", numberOfPlayer, winner))
+		in := userSends("3", "Chris wins")
 
 		game := &SpyGame{}
 
 		cli := poker.NewCLI(in, dummyStdOut, game)
 		cli.PlayPoker()
 
-		if game.StartedWith != numberOfPlayer {
-			t.Errorf("wanted game to start with %d, got %d", numberOfPlayer, game.StartedWith)
-		}
-
-		if game.FinishedWith != winner {
-			t.Errorf("wanted game to finish with %s as winner, got %s", winner, game.FinishedWith)
-		}
+		assertGameStartedWith(t, game, 3)
+		assertGameFinishedWith(t, game, "Chris")
 	})
 	t.Run("print error when non-numeric player is input", func(t *testing.T) {
-		in := strings.NewReader("NotNum\n")
+		in := userSends("NotInt")
 
 		stdOut := &bytes.Buffer{}
 		game := &SpyGame{}
@@ -56,13 +44,8 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(in, stdOut, game)
 		cli.PlayPoker()
 
-		if game.StartCalled {
-			t.Error("game should not have started")
-		}
-
-		gotPrompt := stdOut.String()
-		wantPrompt := poker.PlayerPrompt + poker.BadPlayerInputErrMsg
-		poker.AssertEqual(t, gotPrompt, wantPrompt)
+		assertGameNotStart(t, game)
+		assertMessageSentToUser(t, stdOut, poker.PlayerPrompt, poker.BadPlayerInputErrMsg)
 	})
 }
 
@@ -104,4 +87,35 @@ func (s *SpyGame) Start(numberOfPlayer int) {
 
 func (s *SpyGame) Finish(winner string) {
 	s.FinishedWith = winner
+}
+
+func userSends(inputs ...string) *strings.Reader {
+	return strings.NewReader(strings.Join(inputs, "\n"))
+}
+
+func assertGameStartedWith(t testing.TB, game *SpyGame, numberOfPlayer int) {
+	if game.StartedWith != numberOfPlayer {
+		t.Errorf("wanted game to start with %d, got %d", numberOfPlayer, game.StartedWith)
+	}
+}
+
+func assertGameFinishedWith(t testing.TB, game *SpyGame, winner string) {
+	if game.FinishedWith != winner {
+		t.Errorf("wanted game to finish with %s as winner, got %s", winner, game.FinishedWith)
+	}
+}
+
+func assertGameNotStart(t testing.TB, game *SpyGame) {
+	if game.StartCalled {
+		t.Error("game should not have started")
+	}
+}
+
+func assertMessageSentToUser(t testing.TB, stdout *bytes.Buffer, messages ...string) {
+	t.Helper()
+	want := strings.Join(messages, "")
+	got := stdout.String()
+	if got != want {
+		t.Errorf("wanted %s to be sent to stdOut but got %+v", want, messages)
+	}
 }
